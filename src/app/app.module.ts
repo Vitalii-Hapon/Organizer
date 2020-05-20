c150786fc8a61c06399393b83ce4a17b3a3e9593
Vitalii-Hapon <vitali_gapon@gmail.com>
2020-05-20 18:27:23 +0300

    ADD components ADD data services UPDATE 2020.05.20

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CalendarComponent } from './components/calendar/calendar.component';
+import { OrganizerComponent } from './components/organizer/organizer.component';
+import { SelectorComponent } from './components/selector/selector.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendarComponent,
+    OrganizerComponent,
+    SelectorComponent
   ],
   imports: [
     BrowserModule
